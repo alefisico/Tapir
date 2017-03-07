ca45dbc8b992fe5c378590b5feda94342e33cfa2
d-salerno <daniel.salerno@cern.ch>
2017-03-07 16:06:43 +0100

    changes to test scripts, still doesn't work...

--- MEAnalysis/python/test_MEAnalysis_heppy.py
+++ MEAnalysis/python/test_MEAnalysis_heppy.py
@@ -6,8 +6,8 @@ import ROOT
 
 class MEAnalysisTestCase(unittest.TestCase):
     testfiles = [
-        ("/store/user/jpata/tth/tth_Jul31_V24_v1/ttHTobb_M125_13TeV_powheg_pythia8/tth_Jul31_V24_v1/160731_130548/0000/tree_1.root", "tth"),
-        ("/store/user/jpata/tth/Aug11_leptonic_nome_v1/TT_TuneCUETP8M1_13TeV-powheg-pythia8/Aug11_leptonic_nome_v1/160811_212409/0000/tree_1.root", "ttjets")
+        ("/store/user/jpata/tth/Feb1_leptonic_nome/ttHTobb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/Feb1_leptonic_nome/170201_171753/0000/tree_10.root", "tth"),
+        #("/store/user/jpata/tth/Feb6_leptonic_nome/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/Feb6_leptonic_nome/170206_161748/0000/tree_10.root", "ttjets")
     ]
     
     def launch_test_MEAnalysis(self, infile, sample):
@@ -16,7 +16,7 @@ class MEAnalysisTestCase(unittest.TestCase):
         env["ME_CONF"] = os.path.join(CMSSW_BASE, "src/TTH/MEAnalysis/python/cfg_local.py")
         env["INPUT_FILE"] = infile
         env["TTH_SAMPLE"] = sample
-        outdir = "Loop_{0}".format(sample)
+        outdir = "Loop_2_{0}".format(sample) #if this is anything but Loop_{0} the looper will number them Loop_sample_X, otherwise it requires no existing directory
         if os.path.isdir(outdir):
             raise Exception("output directory exists: {0}".format(outdir))
 
