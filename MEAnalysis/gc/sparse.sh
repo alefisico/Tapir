b83ab8b6236e31f563723b525cd7d1278377d084
Joosep Pata <joosep.pata@gmail.com>
2017-03-13 21:25:55 +0100

    local sparsinator running

--- MEAnalysis/gc/sparse.sh
+++ MEAnalysis/gc/sparse.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 source common.sh
 cd $GC_SCRATCH
-#in case of FileBasedSplitter, need to override MAX_EVENTS
-export MAX_EVENTS=999999999999
-export SKIP_EVENTS=0
-ANALYSIS_CONFIG=${CMSSW_BASE}/src/TTH/Plotting/python/Datacards/config_sldl.cfg python ${CMSSW_BASE}/src/TTH/Plotting/python/joosep/sparsinator.py
+ANALYSIS_CONFIG=${CMSSW_BASE}/src/TTH/MEAnalysis/data/default.cfg python ${CMSSW_BASE}/src/TTH/Plotting/python/joosep/sparsinator.py
