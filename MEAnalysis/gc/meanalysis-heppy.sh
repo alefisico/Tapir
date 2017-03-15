1c9e386943ce03862689f24e71bf1ed5f7b54a1b
d-salerno <daniel.salerno@cern.ch>
2017-03-15 16:04:48 +0100

    remove unessary line in meanalysis-heppy.sh

--- MEAnalysis/gc/meanalysis-heppy.sh
+++ MEAnalysis/gc/meanalysis-heppy.sh
@@ -7,7 +7,6 @@
 #export DATASETPATH=ttHTobb_M125_13TeV_powheg_pythia8
 #export FILE_NAMES=/store/user/jpata/VHBBHeppyV20/ttHTobb_M125_13TeV_powheg_pythia8/VHBB_HEPPY_V20_ttHTobb_M125_13TeV_powheg_Py8__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160209_170826/0000/tree_1.root
 #export GC_SCRATCH=./
-export ME_CONF=$CMSSW_BASE/src/TTH/MEAnalysis/python/@me_conf@
 
 source common.sh
 
