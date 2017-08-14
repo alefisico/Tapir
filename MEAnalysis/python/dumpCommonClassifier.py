64c269241922366549ac2859a798186adff54c6e
Maren Meinhard <your e-mail>
2017-08-14 11:19:29 +0200

    Update dumpCommonClassifier

--- MEAnalysis/python/dumpCommonClassifier.py
+++ MEAnalysis/python/dumpCommonClassifier.py
@@ -4,58 +4,98 @@ from TTH.MEAnalysis.samples_base import getSitePrefix, get_prefix_sample
 from PhysicsTools.HeppyCore.statistics.tree import Tree
 import numpy as np
 
-class Correction:
-    def __init__(self, *args, **kwargs):
-        self.name = kwargs.get("name")
-        self.nominal = kwargs.get("nominal")
-        self.variated = kwargs.get("variated")
-
 class Jet:
     def __init__(self, *args, **kwargs):
         self.pt = kwargs.get("pt")
         self.eta = kwargs.get("eta")
         self.phi = kwargs.get("phi")
         self.mass = kwargs.get("mass")
+        self.corr = kwargs.get("corr")
+        self.corr_JER = kwargs.get("corr_JER")
+        for v in jet_corrections:
+            for ud in ["Up","Down"]:
+                setattr(self,"{a}{b}".format(a = v,b = ud),kwargs.get("{}{}".format(v,ud)))
 
         self.csv = kwargs.get("csv")
         self.cmva = kwargs.get("cmva")
-        self.corrections = kwargs.get("corrections")
+
 
     def correct(self, correction):
-        return Jet(
-            pt = correction.variated * self.pt / correction.nominal if correction.nominal > 0 else self.pt,
-            eta = self.eta,
-            phi = self.phi,
-            mass = correction.variated * self.mass / correction.nominal if correction.nominal > 0 else self.mass,
-            csv = self.csv,
-            cmva = self.cmva,
-            corrections = self.corrections
-        )
 
-def make_corrections(event, schema, jet_base, ijet):
-    corrs = []
+        dic =  {}
+        dic["pt"] = self.pt
+        dic["eta"] = self.eta
+        dic["phi"] = self.phi
+        dic["mass"] = self.mass
+        dic["csv"] = self.csv
+        dic["cmva"] = self.cmva
+        dic["corr"] = self.corr
+        dic["corr_JER"] = self.corr_JER
 
-    corrs += [Correction(
-        name = "nominal",
-        nominal=1.0,
-        variated=1.0,
-    )]
+        for v in jet_corrections:
+            for ud in ["Up","Down"]:
+                dic["{a}{b}".format(a = v,b = ud)] = getattr(self,"{a}{b}".format(a = v,b = ud))
 
-    if schema == "mc":
-        for nominal, variated in [
-            ("corr", "JESUp"),
-            ("corr", "JESDown"),
-            ("corr_JER", "JERUp"),
-            ("corr_JER", "JERDown"),
-        ]:
-            corrs += [
-                Correction(
-                    name = variated,
-                    nominal=getattr(event, "{0}_{1}".format(jet_base, nominal))[ijet],
-                    variated=getattr(event, "{0}_{1}".format(jet_base, "corr_" + variated))[ijet],
-                )
-            ]
-    return corrs
+        return Jet(**dic)
+           
+
+jet_corrections = [        
+            "AbsoluteStat",
+            "AbsoluteScale",
+            "AbsoluteFlavMap",
+            "AbsoluteMPFBias",
+            "Fragmentation",
+            "SinglePionECAL",
+            "SinglePionHCAL",
+            "FlavorQCD",
+            "TimePtEta",
+            "RelativeJEREC1",
+            "RelativeJEREC2",
+            "RelativeJERHF",
+            "RelativePtBB",
+            "RelativePtEC1",
+            "RelativePtEC2",
+            "RelativePtHF",
+            "RelativeBal",
+            "RelativeFSR",
+            "RelativeStatFSR",
+            "RelativeStatEC",
+            "RelativeStatHF",
+            "PileUpDataMC",
+            "PileUpPtRef",
+            "PileUpPtBB",
+            "PileUpPtEC1",
+            "PileUpPtEC2",
+            "PileUpPtHF",
+            "PileUpMuZero",
+            "PileUpEnvelope",
+            "SubTotalPileUp",
+            "SubTotalRelative",
+            "SubTotalPt",
+            "SubTotalScale",
+            "SubTotalAbsolute",
+            "SubTotalMC",
+            "Total",
+            "TotalNoFlavor",
+            "TotalNoTime",
+            "TotalNoFlavorNoTime",
+            "FlavorZJet",
+            "FlavorPhotonJet",
+            "FlavorPureGluon",
+            "FlavorPureQuark",
+            "FlavorPureCharm",
+            "FlavorPureBottom",
+            "TimeRunBCD",
+            "TimeRunEF",
+            "TimeRunG",
+            "TimeRunH",
+            "CorrelationGroupMPFInSitu",
+            "CorrelationGroupIntercalibration",
+            "CorrelationGroupbJES",
+            "CorrelationGroupFlavor",
+            "CorrelationGroupUncorrelated",
+            "JER"
+        ]
 
 class Scenario:
     def __init__(self, *args, **kwargs):
@@ -74,42 +114,53 @@ if __name__ == "__main__":
         prefix, sample_name = get_prefix_sample(os.environ["DATASETPATH"])
         an_name, analysis = analysisFromConfig(os.environ.get("ANALYSIS_CONFIG"))
     else:
+        #file_names = ["/mnt/t3nfs01/data01/shome/mameinha/tth/data/marchsamples.root"]
         file_names = map(getSitePrefix, [
-            "/store/user/jpata/tth/Sep14_leptonic_nome_v1/ttHTobb_M125_13TeV_powheg_pythia8/Sep14_leptonic_nome_v1/160914_142604/0000/tree_1.root"
+            "/mnt/t3nfs01/data01/shome/mameinha/tth/gc/meanalysis/GCd1660f1280ba/ttHTobb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/job_13_out.root"
         ])
         prefix = ""
-        sample_name = "ttHTobb_M125_13TeV_powheg_pythia8"
-        an_name, analysis = analysisFromConfig(os.environ["CMSSW_BASE"] + "/src/TTH/Plotting/python/Datacards/config_sldl.cfg")
+        sample_name = "ttHTobb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8"
+        analysis = analysisFromConfig(os.environ["CMSSW_BASE"] + "/src/TTH/MEAnalysis/data/default.cfg")
 
     sample = analysis.get_sample(sample_name)
 
     ch = ROOT.TChain("tree")
     for fi in file_names:
-        ch.AddFile(getSitePrefix(fi))
-    
+        ch.AddFile(fi)
+
     outfile = ROOT.TFile('out.root', 'recreate')
     tree = Tree('tree', 'MEM tree')
-    tree.var('systematic', type=int)
-    tree.var('njets', type=int)
+    tree.var('systematic', the_type=int)
+    tree.var('njets', the_type=int)
     max_jets = 10
     for v in ["jet_pt", "jet_eta", "jet_phi", "jet_mass", "jet_csv", "jet_cmva"]:
-        tree.vector(v, "njets", maxlen=max_jets, type=float, storageType="F")
+        tree.vector(v, "njets", maxlen=max_jets, the_type=float, storageType="F")
 
     for v in ["jet_type"]:
-        tree.vector(v, "njets", maxlen=max_jets, type=int, storageType="i")
+        tree.vector(v, "njets", maxlen=max_jets, the_type=int, storageType="i")
+
+    for v in ["jet_corr"]:
+        tree.vector(v, "njets", maxlen=max_jets, the_type=float, storageType="F")
+
+    for v in ["jet_corr_JER"]:
+        tree.vector(v, "njets", maxlen=max_jets, the_type=float, storageType="F")
+
+    for v in jet_corrections:
+        for ud in ["Up","Down"]:
+            tree.vector("jet_{}{}".format(v,ud), "njets", maxlen=max_jets, the_type=float, storageType="F")
     
     max_leps = 2
-    tree.var('nleps', type=int)
+    tree.var('nleps', the_type=int)
     for v in ["lep_pt", "lep_eta", "lep_phi", "lep_mass", "lep_charge"]:
-        tree.vector(v, "nleps", maxlen=max_leps, type=float, storageType="F")
+        tree.vector(v, "nleps", maxlen=max_leps, the_type=float, storageType="F")
     
-    tree.var('met_pt', type=float, storageType="F")
-    tree.var('met_phi', type=float, storageType="F")
+    tree.var('met_pt', the_type=float, storageType="F")
+    tree.var('met_phi', the_type=float, storageType="F")
     
-    tree.var('hypothesis', type=int, storageType="I")
+    tree.var('hypothesis', the_type=int, storageType="I")
    
     for v in ["event", "run", "lumi"]:
-        tree.var(v, type=int, storageType="L")
+        tree.var(v, the_type=int, storageType="L")
 
     for iEv, ev in enumerate(ch):
         accept = (ev.is_sl and ev.njets >= 4 and (ev.nBCSVM >= 3 or ev.nBCMVAM >= 3))
@@ -129,32 +180,69 @@ if __name__ == "__main__":
                 ev.leps_mass[ilep]
             ]
             leps_p4 += [p4]
-            leps_charge += [math.copysign(1, ev.leps_pdgId[ilep])]
+            #Minus sign because in pdg particles are positive and antiparticle negative
+            leps_charge += [math.copysign(1, -ev.leps_pdgId[ilep])]
  
         jets = []
 
         for ijet in range(ev.njets)[:max_jets]:
-            jets += [Jet(
-                pt = ev.jets_pt[ijet],
-                eta = ev.jets_eta[ijet],
-                phi = ev.jets_phi[ijet],
-                mass = ev.jets_mass[ijet],
-                csv = ev.jets_btagCSV[ijet],
-                cmva = ev.jets_btagCMVA[ijet],
-                corrections = make_corrections(ev, sample.schema, "jets", ijet)
-            )]
+
+            dic =  {}
+            dic["pt"] = ev.jets_pt[ijet]
+            dic["eta"] = ev.jets_eta[ijet]
+            dic["phi"] = ev.jets_phi[ijet]
+            dic["mass"] = ev.jets_mass[ijet]
+            dic["csv"] = ev.jets_btagCSV[ijet]
+            dic["cmva"] = ev.jets_btagCMVA[ijet]
+            dic["corr"] = ev.jets_corr[ijet]
+            dic["corr_JER"] = ev.jets_corr_JER[ijet]
+
+            for v in jet_corrections:
+                for ud in ["Up","Down"]:
+                    dic["{a}{b}".format(a = v,b = ud)] = getattr(ev,"jets_corr_{a}{b}".format(a = v,b = ud))[ijet]
+
+            jets += [Jet(**dic)]
+
+        for ijet in range(ev.nloose_jets)[:max_jets-ev.njets]:
+
+            dic =  {}
+            dic["pt"] = ev.loose_jets_pt[ijet]
+            dic["eta"] = ev.loose_jets_eta[ijet]
+            dic["phi"] = ev.loose_jets_phi[ijet]
+            dic["mass"] = ev.loose_jets_mass[ijet]
+            dic["csv"] = ev.loose_jets_btagCSV[ijet]
+            dic["cmva"] = ev.loose_jets_btagCMVA[ijet]
+            dic["corr"] = ev.loose_jets_corr[ijet]
+            dic["corr_JER"] = ev.loose_jets_corr_JER[ijet]
+
+            for v in jet_corrections:
+                for ud in ["Up","Down"]:
+                    dic["{a}{b}".format(a = v,b = ud)] = getattr(ev,"loose_jets_corr_{a}{b}".format(a = v,b = ud))[ijet]
+
+            jets += [Jet(**dic)]
 
         scenarios = []
-        for isf in range(len(jets[0].corrections)):
-            scenario = Scenario(
-                jets = [j.correct(j.corrections[isf]) for j in jets],
+        #for isf in range(len(jets[0].corrections)):
+        #    scenario = Scenario(
+        #        jets = [j.correct(j.corrections[isf]) for j in jets],
+        #        leps_p4 = leps_p4,
+        #        leps_charge = leps_charge,
+        #        met_pt = ev.met_pt,
+        #        met_phi = ev.met_phi,
+        #        systematic_index = isf
+        #    )
+        #    scenarios += [scenario]
+
+        scenario = Scenario(
+                jets = [j for j in jets],
                 leps_p4 = leps_p4,
                 leps_charge = leps_charge,
                 met_pt = ev.met_pt,
                 met_phi = ev.met_phi,
-                systematic_index = isf
+                systematic_index = 0
             )
-            scenarios += [scenario]
+
+        scenarios += [scenario]
 
         for scenario in scenarios:
             tree.fill('njets', len(scenario.jets))
@@ -164,6 +252,11 @@ if __name__ == "__main__":
             tree.vfill('jet_mass', [x.mass for x in scenario.jets])
             tree.vfill('jet_csv', [x.csv for x in scenario.jets])
             tree.vfill('jet_cmva', [x.cmva for x in scenario.jets])
+            tree.vfill('jet_corr', [x.corr for x in scenario.jets])
+            tree.vfill('jet_corr_JER', [x.corr_JER for x in scenario.jets])
+            for unc in jet_corrections:
+                for ud in ["Up","Down"]:
+                    tree.vfill('jet_{}{}'.format(unc,ud), [getattr(x,"{}{}".format(unc,ud)) for x in scenario.jets])
 
             tree.fill('nleps', len(scenario.leps_p4))
             tree.vfill('lep_pt', [x[0] for x in scenario.leps_p4])
