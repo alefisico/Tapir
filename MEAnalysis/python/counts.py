a9a19115bbb3bccd0e7f7b1656e2154e35e993ab
Joosep Pata <joosep.pata@gmail.com>
2018-01-23 14:39:24 +0100

    work on ngen counting

--- MEAnalysis/python/counts.py
+++ MEAnalysis/python/counts.py
@@ -3,61 +3,25 @@ import sys
 from TTH.MEAnalysis.samples_base import getSitePrefix
 
 
-def get_tree_entries(treename, filenames):
-    tt = ROOT.TChain(treename)
-    for fi in filenames:
-        print "adding", fi
-        tt.AddFile(fi)
-    i = tt.GetEntries()
-    return i
-
 def main(filenames, ofname):
     filenames_pref = map(getSitePrefix, filenames)
     of = ROOT.TFile(ofname, "RECREATE")
-    good_filenames = []
-    count_dict = {}
-    count_dict["Count"] = 0
+    tree_list = ROOT.TList()
 
-    count_dict["failed"] = []
-    count_dict["good_files"] = []
+    #keep tfiles open for the duration of the merge
+    tfiles = []
     for infn, lfn in zip(filenames_pref, filenames):
-        print "trying to open {0}".format(infn)
         tf = ROOT.TFile.Open(infn)
-        
-        print "good file", infn, tf
-        good_filenames += [lfn]
-        vhbb_dir = tf.Get("vhbb")
-        if not vhbb_dir:
-            print "could not find vhbb directory" 
-            vhbb_dir = tf
-        print [k.GetName() for k in vhbb_dir.GetListOfKeys()]
+        tfiles += [tf]
 
-        for k in vhbb_dir.GetListOfKeys():
-            kn = k.GetName()
-            print kn
-            if "Count" in kn:
-                o = k.ReadObj()
-                if not count_dict.has_key(kn):
-                    count_dict[kn] = 0
-                count_dict[kn] += o.GetBinContent(1)
-                if not of.Get(kn):
-                    print "first file, creating histogram", kn
-                    o2 = o.Clone()
-                    of.Add(o2)
-                else:
-                    of.Get(kn).Add(o)
-        of.Write()
-        tf.Close()
-    count_dict["good_files"] = good_filenames
-    
-    #hEntries = ROOT.TH1D("numEntries", "numEntries", 3, 0, 3)
-    #hEntries.SetDirectory(of)
-    #hEntries.SetBinContent(1, get_tree_entries("vhbb/tree", good_filenames))
-    #hEntries.SetBinContent(2, get_tree_entries("tree", good_filenames))
-    #hEntries.Write()
-    
+        run_tree = tf.Get("Runs")
+        tree_list.Add(run_tree)
+
+    of.cd()
+    out_tree = ROOT.TTree.MergeTrees(tree_list)
+    out_tree.Write()
     of.Close()
-    return count_dict
+    return ofname
 
 if __name__ == "__main__":
     filenames = sys.argv[2:]
--- MEAnalysis/python/getCounts.py
+++ MEAnalysis/python/getCounts.py
@@ -1,21 +1,11 @@
 import ROOT, sys
 
-d = {}
-d2 = {}
-d3 = {}
+
 for fn in sys.argv[1:]:
     f = ROOT.TFile(fn)
-    
-    h = f.Get("Count")
-    d[fn.split("/")[-1].split(".")[0]] = h.GetBinContent(1)
-    
-    
-    h1 = f.Get("CountPosWeight")
-    h2 = f.Get("CountNegWeight")
-    d2[fn.split("/")[-1].split(".")[0]] = h1.GetBinContent(1) - h2.GetBinContent(1)
-    
-    h = f.Get("CountFullWeighted")
-    d3[fn.split("/")[-1].split(".")[0]] = h.GetBinContent(1)
-
-for k in sorted(d.keys()):
-    print k, d[k], d2[k], d3[k]
+    countw = 0
+    tree = f.Get("Runs")
+    for ient in range(tree.GetEntries()):
+        tree.GetEntry(ient)
+        countw += tree.genEventSumw
+    print fn, countw
