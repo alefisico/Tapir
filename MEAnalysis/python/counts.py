38df9acf7370bc3c9ba2f8f43dfcd571ea1ff1d4
Joosep Pata <joosep.pata@gmail.com>
2018-02-02 10:00:13 +0100

    comments

--- MEAnalysis/python/counts.py
+++ MEAnalysis/python/counts.py
@@ -1,9 +1,26 @@
+#!/usr/bin/env python
+"""
+Retrieves the number of generated MC events from the input files and saves it
+into an output file.
+In nanoAOD, this number is stored in a separate TTree "Runs", which can simply
+be added up from all the input files.
+"""
 import ROOT
 import sys
 from TTH.MEAnalysis.samples_base import getSitePrefix
 
 
 def main(filenames, ofname):
+    """
+    Retrieves counts from input files and saves to output file.
+
+    Args:
+        filenames (list of strings): Input ROOT file names
+        ofname (string): Output ROOT file name, will be created
+    
+    Returns:
+        Output file name
+    """
     filenames_pref = map(getSitePrefix, filenames)
     of = ROOT.TFile(ofname, "RECREATE")
     tree_list = ROOT.TList()
