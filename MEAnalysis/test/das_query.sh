d913b7a2ce886b14914d7a629593363a568bbc57
Joosep Pata <joosep.pata@gmail.com>
2018-02-02 17:25:12 +0100

    add input nanoaod datasets

--- MEAnalysis/test/das_query.sh
+++ MEAnalysis/test/das_query.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#run as ./das_query.txt < dataset.txt | sed 's/root/root = 1/' > dataset_files.txt
+#run as ./das_query.sh < dataset.txt | sed 's/root/root = 1/' > dataset_files.txt
 while read dataset; do
     dasgoclient -query="file dataset=$dataset instance=prod/phys03"
 done
