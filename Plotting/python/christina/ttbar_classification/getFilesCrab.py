cbeff063c4bc59f285068d5b37cd177b9727b041
Joosep Pata <joosep.pata@gmail.com>
2018-05-17 15:52:54 +0200

    fix pnfs path

--- Plotting/python/christina/ttbar_classification/getFilesCrab.py
+++ Plotting/python/christina/ttbar_classification/getFilesCrab.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="collect crab_nano output and make dataset .txt files")
     parser.add_argument("--path", required=True, help="path of crab output, e.g. /pnfs/psi.ch/cms/trivcat/store/user/algomez/tth/May08_t1/", type=str)
     parser.add_argument("--outpath", default="/mnt/t3nfs01/data01/shome/creissel/tth/2017/sw/CMSSW_9_4_5_cand1/src/TTH/MEAnalysis/gc/datasets", help="path to store dataset .txt files", type=str)
-    parser.add_argument("--prefix", default="root://t3dcachedb.psi.ch", help="server prefix to add to rootfiles", type=str)
+    parser.add_argument("--prefix", default="root://t3dcachedb.psi.ch/", help="server prefix to add to rootfiles", type=str)
     parser.add_argument("--treename", default="nanoAOD/Events", help="Name of the TTree to retrieve the number of events from", type=str)
     args = parser.parse_args()
 
--- Plotting/python/joosep/controlPlot.py
+++ Plotting/python/joosep/controlPlot.py
@@ -1,4 +1,7 @@
 print "starting"
+import logging
+import multiprocessing
+
 import math
 import json
 
@@ -52,18 +55,18 @@ procs = [x[0] for x in procs_names]
 syst_pairs = []
 syst_pairs.extend([
 #    ("__CMS_puUp", "__CMS_puDown"),
-#    ("__CMS_scale_jUp", "__CMS_scale_jDown"),
+    ("__CMS_scale_jUp", "__CMS_scale_jDown"),
 #    ("__CMS_scaleFlavorQCD_jUp", "__CMS_scaleFlavorQCD_jDown"),
-#    ("__CMS_res_jUp", "__CMS_res_jDown"),
-#    ("__CMS_ttH_CSVcferr1Up", "__CMS_ttH_CSVcferr1Down"),
-#    ("__CMS_ttH_CSVcferr2Up", "__CMS_ttH_CSVcferr2Down"),
-#    ("__CMS_ttH_CSVhfUp", "__CMS_ttH_CSVhfDown"),
-#    ("__CMS_ttH_CSVhfstats1Up", "__CMS_ttH_CSVhfstats1Down"),
-#    ("__CMS_ttH_CSVhfstats2Up", "__CMS_ttH_CSVhfstats2Down"),
-#    ("__CMS_ttH_CSVjesUp", "__CMS_ttH_CSVjesDown"),
-#    ("__CMS_ttH_CSVlfUp", "__CMS_ttH_CSVlfDown"),
-#    ("__CMS_ttH_CSVlfstats1Up", "__CMS_ttH_CSVlfstats1Down"),
-#    ("__CMS_ttH_CSVlfstats2Up", "__CMS_ttH_CSVlfstats2Down"),
+    ("__CMS_res_jUp", "__CMS_res_jDown"),
+    ("__CMS_ttH_CSVcferr1Up", "__CMS_ttH_CSVcferr1Down"),
+    ("__CMS_ttH_CSVcferr2Up", "__CMS_ttH_CSVcferr2Down"),
+    ("__CMS_ttH_CSVhfUp", "__CMS_ttH_CSVhfDown"),
+    ("__CMS_ttH_CSVhfstats1Up", "__CMS_ttH_CSVhfstats1Down"),
+    ("__CMS_ttH_CSVhfstats2Up", "__CMS_ttH_CSVhfstats2Down"),
+    ("__CMS_ttH_CSVjesUp", "__CMS_ttH_CSVjesDown"),
+    ("__CMS_ttH_CSVlfUp", "__CMS_ttH_CSVlfDown"),
+    ("__CMS_ttH_CSVlfstats1Up", "__CMS_ttH_CSVlfstats1Down"),
+    ("__CMS_ttH_CSVlfstats2Up", "__CMS_ttH_CSVlfstats2Down"),
 #    ("__CMS_ttjetsisrUp", "__CMS_ttjetsisrDown"),
 #    ("__CMS_ttjetsfsrUp", "__CMS_ttjetsfsrDown"),
 #    ("__CMS_ttjetstuneUp", "__CMS_ttjetstuneDown"),
@@ -264,7 +267,8 @@ def get_base_plot(basepath, outpath, analysis, category, variable):
     return ret
 
 if __name__ == "__main__":
-
+    
+    logging.basicConfig(level=logging.INFO)
 
     # Plot for all SL categories
     simple_vars = [
@@ -276,7 +280,7 @@ if __name__ == "__main__":
 #        "jetsByPt_1_btagCSV",
 #        "jetsByPt_2_btagCSV",
 #        "jetsByPt_3_btagCSV",
-#        "jetsByPt_0_pt",
+        "jetsByPt_0_pt",
 #        "jetsByPt_1_pt",
 #        "jetsByPt_2_pt",
 #        "jetsByPt_3_pt",
@@ -284,16 +288,20 @@ if __name__ == "__main__":
 #        "jetsByPt_1_eta",
 #        "jetsByPt_2_eta",
 #        "jetsByPt_3_eta",
-#        "leps_0_pt"
+        "leps_0_pt"
     ]
 
     cats = [
         ("sl_jge4_tge2", simple_vars),
         ("dl_jge4_tge2", simple_vars),
-#        ("sl_jge6_t3", ["jetsByPt_0_pt", "btag_LR_4b_2b_btagCSV_logit"]),
-#        ("dl_jge4_t3", ["jetsByPt_0_pt", "btag_LR_4b_2b_btagCSV_logit"]),
-#        ("sl_jge6_tge4", ["jetsByPt_0_pt", "mem_SL_2w2h2t_p"]),
-#        ("dl_jge4_tge4", ["jetsByPt_0_pt", "mem_DL_0w2h2t_p"]),
+        ("dl_jge4_t3", ["jetsByPt_0_pt", "leps_0_pt"]),
+        ("dl_jge4_tge4", ["jetsByPt_0_pt", "leps_0_pt"]),
+        ("sl_jge6_t3", ["jetsByPt_0_pt", "leps_0_pt"]),
+        ("sl_jge6_tge4", ["jetsByPt_0_pt", "leps_0_pt"]),
+        ("sl_j5_t3", ["jetsByPt_0_pt", "leps_0_pt"]),
+        ("sl_j5_tge4", ["jetsByPt_0_pt", "leps_0_pt"]),
+        ("sl_j4_t3", ["jetsByPt_0_pt", "leps_0_pt"]),
+        ("sl_j4_tge4", ["jetsByPt_0_pt", "leps_0_pt"]),
     ]
 
     args = []
@@ -313,6 +321,8 @@ if __name__ == "__main__":
         arg["do_tex"] = False
         if "numJets" in arg["histname"]:
             arg["do_log"] = True
-        print json.dumps(arg, indent=2)
-        plot_worker(arg)
-
+        #plot_worker(arg)
+    
+    pool = multiprocessing.Pool(8)
+    pool.map(plot_worker, args)
+    pool.close()
